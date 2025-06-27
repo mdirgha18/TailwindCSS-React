417a363188f61febfe27572afdc98feb67419b05
mdirgha18 <dirgha.mankad702@gmail.com>
2025-06-27 11:56:14 +0530

    Create "Choose file" styling.tsx

---
+++ "Choose file" styling.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <input
+      type="file"
+      className="file:mr-4 file:rounded-full file:border-0 file:bg-violet-50 file:py-2 file:text-sm file:font-semibold file:text-violet-700 hover:file:bg-violet-100 dark:file:bg-violet-600 dark:file:text-violet-100 dark:hover:file:bg-violet-500 ..."
+    />
+  );
+}
+
+export default App;
