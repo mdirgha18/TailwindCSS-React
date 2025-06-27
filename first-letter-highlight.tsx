7e66111ba1011d3b45097b325b4ee61105e7634b
mdirgha18 <dirgha.mankad702@gmail.com>
2025-06-27 13:34:34 +0530

    Create first-letter-highlight.tsx

---
+++ first-letter-highlight.tsx
@@ -0,0 +1,20 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-900 flex items-center justify-center p-2">
+      <div className="max-w-2xl">
+        <div className="text-white">
+          <p className="first-letter:float-left first-letter:mr-3 first-letter:text-7xl first-letter:font-bold first-letter:text-white">
+            Well, let me tell you something, funny boy. Y'know that little
+            stamp, the one that says "New York Public Library"?
+          </p>
+          <p className="mt-6">
+            Well, that may not mean anything to you, but that means a lot to me.
+            One whole hell a lot.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
