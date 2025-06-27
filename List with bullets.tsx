a90dcdeb00db99914ebb9b15efb68614014d1be7
mdirgha18 <dirgha.mankad702@gmail.com>
2025-06-27 12:07:42 +0530

    Create List with bullets.tsx

---
+++ List with bullets.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-900 flex items-center justify-center">
+      <div className="bg-gray-800 p-6 rounded-lg shadow-lg max-w-sm w-full">
+        <h2 className="text-xl font-bold text-white-white mb-4">Ingredients</h2>
+        <ul role="list" className="list-disc marker:text-sky-400 ...">
+          <li className="text-white">5 cups chopped Porchini mushrooms</li>
+          <li className="text-white">1/2 cup of olive oil</li>
+          <li className="text-white">3lb of celery</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+export default App;
