ada4802b079d9640e3588bd0890cd3160a891b8a
mdirgha18 <dirgha.mankad702@gmail.com>
2025-06-27 11:33:11 +0530

    Create highlighting-text.tsx

---
+++ highlighting-text.tsx
@@ -0,0 +1,14 @@
+
+function App() {
+  return (
+    <blockquote className="text-center text-2xl font-semibold text-gray-900  italic darktext-white">
+      When you look
+      <span className="relative inline-block before:absolute before:inset-1 before:block before:-skew-y-3 before:bg-pink-500">
+        <span className="relative text-white dark:text-gray-950">annoyed</span>
+      </span>
+      all the time, people think that you're busy.
+    </blockquote>
+  );
+}
+
+export default App;
