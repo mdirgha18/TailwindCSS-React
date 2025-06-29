8af7f59eda028961b8a5a37820ce0f81f6bc4c14
mdirgha18 <dirgha.mankad702@gmail.com>
2025-06-30 00:55:21 +0530

    Create simple-button.tsx

---
+++ simple-button.tsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="min-h-screen bg-black flex items-center justify-center">
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold-type py-2 px-4 rounded">
+        Click Me
+      </button>
+    </div>
+  );
+}
+
+export default App;
