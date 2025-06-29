79bca78ca05973f3ffba89048dd727b17a1a84a6
mdirgha18 <dirgha.mankad702@gmail.com>
2025-06-29 20:44:52 +0530

    Create styling-direct-children.tsx

---
+++ styling-direct-children.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+      <h2>Categories</h2>
+      <ul className="*:rounded-full *:border-sky-100 *:bg-sky-50 *:px-2 *:py-0.5 dark:text-sky-300 dark:*:border-sky-500/15 dark:*:bg-sky-500/10">
+        <li>Sales</li>
+        <li>Marketing</li>
+        <li>SEO</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
